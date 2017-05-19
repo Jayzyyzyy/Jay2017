d6c31f9dcf1a15aeeb115d956eeab29528b0ca78
JayXu <xuweijay@163.com>
2017-05-19 15:28:41 +0800

    多线程

---
+++ Java多线程/Chapter3/OneValueCache.java
@@ -0,0 +1,28 @@
+package Chapter3;
+
+import net.jcip.annotations.Immutable;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+/**
+ *  不可变容器类
+ */
+@Immutable
+public class OneValueCache {
+    private final BigInteger lastNumber;
+    private final BigInteger[] lastFactors;
+
+    public OneValueCache(BigInteger i,
+                         BigInteger[] factors) {
+        lastNumber = i;
+        lastFactors = Arrays.copyOf(factors, factors.length);
+    }
+
+    public BigInteger[] getFactors(BigInteger i) {
+        if (lastNumber == null || !lastNumber.equals(i))
+            return null;
+        else
+            return Arrays.copyOf(lastFactors, lastFactors.length);
+    }
+}
---
+++ Java多线程/Chapter3/ThreeStooges.java
@@ -0,0 +1,25 @@
+package Chapter3;
+
+import net.jcip.annotations.Immutable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *  不可变对象
+ */
+@Immutable
+public class ThreeStooges {
+    private final Set<String> stooges = new HashSet<String>();
+
+    public ThreeStooges(){
+        stooges.add("Moe");
+        stooges.add("Larry");
+        stooges.add("Curly");
+    }
+
+    public boolean isStooge(String name){
+        return stooges.contains(name);
+    }
+
+}
