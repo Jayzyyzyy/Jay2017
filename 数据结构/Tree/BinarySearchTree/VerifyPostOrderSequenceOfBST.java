78b8ba6d18cdefe8232e391dca4d23f4f7a19a5a
Jayzyyzyy <xuweijay@163.com>
2017-05-20 10:58:36 +0800

    tree

---
+++ 数据结构/Tree/BinarySearchTree/VerifyPostOrderSequenceOfBST.java
@@ -0,0 +1,13 @@
+package Tree.BinarySearchTree;
+
+/**
+ *  验证一个整数数组是否是二叉搜索树的整数序列
+ */
+public class VerifyPostOrderSequenceOfBST {
+    public static boolean verifySequenceOfBST(int[] sequence, int length){
+
+
+
+        return false;
+    }
+}
