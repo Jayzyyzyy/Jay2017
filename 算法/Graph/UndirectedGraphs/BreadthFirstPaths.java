1ce787e71fedf3c69aea7883d1a0a2ac094b8018
JayXu <xuweijay@163.com>
2017-05-27 10:48:57 +0800

    广度优先搜索，求取最短路径

---
+++ 算法/Graph/UndirectedGraphs/BreadthFirstPaths.java
@@ -0,0 +1,89 @@
+package Graph.UndirectedGraphs;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * 广度优先搜索（队列）
+ */
+public class BreadthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public BreadthFirstPaths(Graph G, int s){
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        validateVertex(s);
+        this.s = s;
+        bfs(G, s);
+    }
+
+    private void bfs(Graph G, int s){
+        Queue<Integer> queue = new Queue<Integer>();
+        marked[s] = true;
+        edgeTo[s] = s;
+        queue.enqueue(s);
+
+        while(!queue.isEmpty()){
+            int v = queue.dequeue();
+            for (int w : G.adj(v)) {
+                if(!marked[w]){
+                    edgeTo[w] = v;
+                    marked[w] = true;
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v){
+        validateVertex(v);
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v){
+        validateVertex(v);
+        if(!marked[v]) return null; //不连通
+
+        Stack<Integer> path = new Stack<Integer>();
+        for (int x = v; x != s; x = edgeTo[x]) {
+            path.push(x);
+        }
+        path.push(s);
+        return path;
+    }
+
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph G = new Graph(in);
+        int s = Integer.parseInt(args[1]);
+        BreadthFirstPaths dfs = new BreadthFirstPaths(G, s);  //图，起点
+
+        for (int v = 0; v < G.V(); v++) {
+            if (dfs.hasPathTo(v)) {  //s与v连通
+                StdOut.printf("%d to %d:  ", s, v);
+                for (int x : dfs.pathTo(v)) {   //stack
+                    if (x == s) StdOut.print(x);
+                    else        StdOut.print("-" + x);
+                }
+                StdOut.println();
+            }
+
+            else {
+                StdOut.printf("%d to %d:  not connected\n", s, v);
+            }
+
+        }
+    }
+
+}
