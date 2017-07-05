fc65bee2775b8ca927c11a6b9d3082eeb26baf1c
JayXu <xuweijay@163.com>
2017-07-05 23:35:20 +0800

    剑指offer

--- 数据结构/Sword2Offer/P10_NumberOf1InBinary.java
+++ 数据结构/Sword2Offer/P10_NumberOf1InBinary.java
@@ -4,15 +4,23 @@ package Sword2Offer;
  * 二进制中1的个数
  */
 public class P10_NumberOf1InBinary {
+    //n二进制整数中有多少个1就只需循环几次
+    public int NumberOf1_3(int n){
+        int count = 0;
+        while(n != 0){ //二进制中还有位为1
+            count ++;
+            n = n & (n-1);  //二进制靠近右边第一个为1的位从1变为0，再与原数与，将原数n减去了一个1位
+        }
+        return count;
+    }
 
 
-
-    //位运算
+    //位运算，n二进制有几位flag就移动几次
     public int NumberOf1_2(int n){
         int count = 0;
         int flag = 1;
         while(flag != 0){  //从最左边高位移除，变为0
-            if((n & flag) == flag){ //与运算结果为flag，说明该位为1
+            if((n & flag) != 0){ //与运算结果不为0，说明该位为1
                 count ++;
             }
             flag = flag << 1; //flag一直右移，与n进行与运算
@@ -20,7 +28,7 @@ public class P10_NumberOf1InBinary {
         return count;
     }
 
-    //O(n) O(n)
+    //O(n) O(n)时间、空间复杂度
     public int NumberOf1(int n) {
         String s = Integer.toBinaryString( n );
 
---
+++ 数据结构/Sword2Offer/P11_Power.java
@@ -0,0 +1,42 @@
+package Sword2Offer;
+
+/**
+ * 数值的整数次方
+ */
+public class P11_Power {
+    public double Power(double base, int exponent) {
+        if(equals(base, 0.0) && exponent<0){ //base为0，ex为负数，无意义
+            return 0.0;
+        }
+
+        if(exponent < 0){
+            return 1.0/powerWithExponent(base, -exponent);
+        }else{
+            return powerWithExponent(base, exponent);
+        }
+    }
+
+    public boolean equals(double num1,double num2){
+        if(num1-num2 >-0.0000001 && num1-num2<0.0000001){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public double powerWithExponent(double base, int exponent){
+        if(exponent == 0){ //指数为0
+            return 1;
+        }
+
+        if(exponent == 1){ //指数为1
+            return base;
+        }
+
+        double result = 1.0;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+}
