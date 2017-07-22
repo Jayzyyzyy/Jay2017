e71f084a725b5a70270cc34506529231198e4b0b
JayXu <xuweijay@163.com>
2017-07-22 16:07:59 +0800

    剑指offer 复杂链表的复制

--- 数据结构/Sword2Offer/P25_PathInTree.java
+++ 数据结构/Sword2Offer/P25_PathInTree.java
@@ -1,8 +1,52 @@
 package Sword2Offer;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
- * 二叉树中和为某一值的路径()
+ * 二叉树中和为某一值的路径(从根节点到叶子结点)
  */
 public class P25_PathInTree {
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(root == null) return result;
 
+        Stack<Integer> path = new Stack<Integer>(); //路径
+        int sum = 0; //和
+
+        find(result, path, sum, target, root);
+
+        return result;
+    }
+
+    /**
+     * 递归函数(前序遍历)
+     * @param result 结果集
+     * @param path 路径
+     * @param sum 和
+     * @param target 目标数值
+     * @param node 本次节点
+     */
+    public void find(ArrayList<ArrayList<Integer>> result, Stack<Integer> path, int sum, int target, TreeNode node){
+        path.push(node.val); //计算路径、和
+        sum += node.val; //int sum1 = sum + node.val;后面改为sum1
+        //找到
+        if(node.left == null && node.right == null && sum == target){
+            ArrayList<Integer> temp = new ArrayList<Integer>(); //得到的路径不用逆序
+            for (Integer i : path) {
+                temp.add(i);
+            }
+            result.add(temp);
+        }
+        //如果左子树不为空，继续检查
+        if(node.left != null){
+            find(result, path, sum, target, node.left);
+        }
+        //右子树不为空，继续检查
+        if(node.right != null){
+            find(result, path, sum, target, node.right);
+        }
+        //本节点检查结束之后，路径需要删除本节点，sum需要减去本节点的值(局部变量)
+        path.pop();
+    }
 }
---
+++ 数据结构/Sword2Offer/P26_CopyComplexList.java
@@ -0,0 +1,73 @@
+package Sword2Offer;
+
+/**
+ * 复杂链表的复制 O(n) O(1)
+ */
+public class P26_CopyComplexList {
+    public RandomListNode Clone(RandomListNode pHead){
+        if(pHead == null) return null;
+
+        cloneNodes(pHead);
+        connectSiblingNodes(pHead);
+        return reconnectNodes(pHead);
+    }
+
+    //原链表插入节点
+    public void cloneNodes(RandomListNode pHead){
+        RandomListNode temp;
+        RandomListNode current = pHead; //原链表目前的节点
+        while(current != null){
+            temp = new RandomListNode(current.label); //新插入节点
+            RandomListNode next = current.next;
+            current.next = temp;
+            temp.next = next;
+            current = next; //原链表下一个节点
+        }
+
+    }
+
+    //设置随机指针random
+    public void connectSiblingNodes(RandomListNode pHead){
+        RandomListNode current = pHead; //原链表目前的节点
+        while(current != null){
+            RandomListNode copiedNode = current.next; //指向新插入的节点
+            if(current.random == null){ //如果原链表随机指针为null
+                copiedNode.random = null;
+            }else {
+                copiedNode.random = current.random.next;
+            }
+            current = copiedNode.next; //原链表下一个节点
+        }
+    }
+
+    //拆散有上面两步得到的链表,分为两个链表
+    public RandomListNode reconnectNodes(RandomListNode pHead){
+        RandomListNode current = pHead;   //原链表头结点
+        RandomListNode copiedCurrent = pHead.next; //新链表当前指针
+        RandomListNode copiedPHead = pHead.next; //新链表头结点
+
+        current.next = copiedCurrent.next; //初始化
+        current = current.next;
+
+        while(current != null){
+            copiedCurrent.next = current.next;
+            copiedCurrent  = copiedCurrent.next;
+            current.next = copiedCurrent.next;
+            current = copiedCurrent.next;
+        }
+
+        return copiedPHead;
+    }
+
+    public class RandomListNode {
+        int label;
+        RandomListNode next = null;
+        RandomListNode random = null;
+
+        RandomListNode(int label) {
+            this.label = label;
+        }
+    }
+
+    /*也可以使用HashMap将原始节点与新节点作为key value映射存在map中，方便设置random指针，空间换时间O(1)*/
+}
