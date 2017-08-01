18453db1d1c83d1b8e9f163786c729240e107620
Jayzyyzyy <xuweijay@163.com>
2017-08-01 14:02:51 +0800

    dp

---
+++ 数据结构/Nowcoder/DP/Poj1088.java
@@ -0,0 +1,72 @@
+package Nowcoder.DP;
+
+import java.util.Scanner;
+
+/**
+ * 滑雪
+ */
+public class Poj1088 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maxLength = 0;
+        while(sc.hasNextInt()){
+            int M = sc.nextInt();
+            int N = sc.nextInt();
+            int[][] height = new int[M + 2][N + 2];
+            int[][] length = new int[M + 2][N + 2];
+            for (int i = 1; i <= M; i++) {
+                for (int j = 1; j <= N; j++) {
+                    height[i][j] = sc.nextInt();
+                }
+            }
+
+            for (int i = 1; i <= M; i++) {
+                for (int j = 1; j <= N; j++) {
+                    length[i][j] = recursion(height, length, i, j, M, N);
+                    if(maxLength < length[i][j]){
+                        maxLength = length[i][j];
+                    }
+                }
+            }
+            System.out.println(maxLength);
+            maxLength = 0;
+        }
+    }
+    //i,j 的最长路径
+    public static int recursion(int[][] height, int[][] length, int i, int j, int M, int N){
+        if(height == null || height.length == 0 || height[0].length == 0){
+            return 0;
+        }
+        if(i < 1 || j < 1 || i > M || j > N){
+            return 0;
+        }
+
+        if(length[i][j] != 0){
+            return length[i][j];
+        }
+
+        int temp = 0, max = 0;
+        //left
+        if(height[i][j] > height[i-1][j]){
+            temp = recursion(height, length, i-1, j, M, N);
+            max = Math.max(max, temp);
+        }
+        //right
+        if(height[i][j] > height[i+1][j]){
+            temp = recursion(height, length, i+1, j, M, N);
+            max = Math.max(max, temp);
+        }
+        //up
+        if(height[i][j] > height[i][j-1]){
+            temp = recursion(height, length, i, j-1, M, N);
+            max = Math.max(max, temp);
+        }
+        //down
+        if(height[i][j] > height[i][j+1]){
+            temp = recursion(height, length, i, j+1, M, N);
+            max = Math.max(max, temp);
+        }
+
+        return max +1;
+    }
+}
---
+++ 数据结构/Nowcoder/DP/Poj2533.java
@@ -0,0 +1,64 @@
+package Nowcoder.DP;
+
+import java.util.Scanner;
+
+/**
+ * Longest Ordered Subsequence 最长递增子序列
+ */
+public class Poj2533 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(maxLength(arr));
+        }
+    }
+
+    public static int maxLength(int[] arr){
+        if(arr == null || arr.length == 0) return 0;
+
+        int maxLength = 1;
+        int[] dp = new int[arr.length];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < i; j++) {
+                if(arr[i] >= arr[j]){
+                    temp = dp[j] + 1;
+                    dp[i] = Math.max(dp[i], temp);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+
+    public int findLongest(int[] A, int n) {
+        if(A == null || A.length == 0 || n <= 0) return 0;
+
+        int maxLength = 1;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int temp = 0;
+            for (int j = 0; j < i; j++) {
+                if(A[i] >= A[j]){
+                    temp = dp[j] + 1;
+                    dp[i] = Math.max(dp[i], temp);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+}
