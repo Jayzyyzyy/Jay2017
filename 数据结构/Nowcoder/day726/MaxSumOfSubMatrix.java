5ced39abc4d2735df6039df4df970f5798dda586
Jayzyyzyy <xuweijay@163.com>
2017-08-01 09:23:42 +0800

    子矩阵的最大累加和

--- 数据结构/Nowcoder/day726/MaxSumOfSubMatrix.java
+++ 数据结构/Nowcoder/day726/MaxSumOfSubMatrix.java
@@ -2,7 +2,10 @@ package Nowcoder.day726;
 
 import java.util.Scanner;
 
-public class Main {
+/**
+ * 子矩阵的最大累加和
+ */
+public class MaxSumOfSubMatrix {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextInt()){
@@ -18,23 +21,24 @@ public class Main {
             System.out.println(maxSum);
         }
     }
-
+    //子矩阵的最大累加和
     public static int maxSum(int[][] m){
         if(m == null || m.length == 0 || m[0].length == 0) return 0;
 
-        int N = m.length;
-        int maxSum = Integer.MIN_VALUE;
-        int cur = 0;
-        int[] sums = null;
+        int M = m.length; //行
+        int N = m[0].length; //列
+        int maxSum = Integer.MIN_VALUE; //最大和
+        int cur = 0; //记录局部最大
+        int[] sums = null; //累加和
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < M; i++) {
             sums = new int[N];
-            for (int j = i; j < N; j++) {
+            for (int j = i; j < M; j++) {
                 cur = 0;
                 for (int k = 0; k < N; k++) {
                     sums[k] += m[j][k];
                 }
-                for (int k = 0; k < N; k++) {
+                for (int k = 0; k < N; k++) { //一次累加计算局部最大
                     if(cur < 0){
                         cur = sums[k];
                     }else {
