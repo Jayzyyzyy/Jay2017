e0c7036774e2505ea1112b87f85cb5dd1c821d3b
JayXu <xuweijay@163.com>
2017-08-06 16:59:27 +0800

    牛客 8月2日

---
+++ 数据结构/Nowcoder/day0802/Problem_02_Islands.java
@@ -0,0 +1,58 @@
+package Nowcoder.day0802;
+
+/**
+ * dfs连通分量个数
+ */
+public class Problem_02_Islands {
+	//计算岛屿个数
+	public static int countIslands(int[][] m) {
+		if (m == null || m[0] == null) {
+			return 0;
+		}
+		int N = m.length;
+		int M = m[0].length;
+		int res = 0;  //共有几个岛屿
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (m[i][j] == 1) { // 未感染， 0/2(已感染)跳过
+					res++;
+					infect(m, i, j, N, M); //感染一大片(一个连通分量)
+				}
+			}
+		}
+		return res;
+	}
+	//感染
+	public static void infect(int[][] m, int i, int j, int N, int M) {
+		if (i < 0 || i >= N || j < 0 || j >= M || m[i][j] != 1) { // m[i][j] == 0或2，返回
+			return;
+		}
+		m[i][j] = 2;
+		infect(m, i + 1, j, N, M); //下
+		infect(m, i - 1, j, N, M); //上
+		infect(m, i, j + 1, N, M); //右
+		infect(m, i, j - 1, N, M); //左
+	}
+
+	public static void main(String[] args) {
+		int[][] m1 = {  { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+				        { 0, 1, 1, 1, 0, 1, 1, 1, 0 }, 
+				        { 0, 1, 1, 1, 0, 0, 0, 1, 0 },
+				        { 0, 1, 1, 0, 0, 0, 0, 0, 0 }, 
+				        { 0, 0, 0, 0, 0, 1, 1, 0, 0 }, 
+				        { 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+				        { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+		System.out.println(countIslands(m1));
+
+		int[][] m2 = {  { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+						{ 0, 1, 1, 1, 1, 1, 1, 1, 0 }, 
+						{ 0, 1, 1, 1, 0, 0, 0, 1, 0 },
+						{ 0, 1, 1, 0, 0, 0, 1, 1, 0 }, 
+						{ 0, 0, 0, 0, 0, 1, 1, 0, 0 }, 
+						{ 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+						{ 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+		System.out.println(countIslands(m2));
+
+	}
+
+}
