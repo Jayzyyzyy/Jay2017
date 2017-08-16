e2b731f0142ab3c0c9c88508cc813218ff1af4c2
Jayzyyzyy <xuweijay@163.com>
2017-08-16 17:42:47 +0800

    LeetCode

--- 数据结构/LeetCode/BFS/Ex111_Minimum_Depth_of_Binary_Tree.java
+++ 数据结构/LeetCode/BFS/Ex111_Minimum_Depth_of_Binary_Tree.java
@@ -21,6 +21,7 @@ public class Ex111_Minimum_Depth_of_Binary_Tree {
 
         while(!stack.isEmpty()){
             cur = stack.pop();
+
             if(cur.left != null){
                 stack.push(cur.left);
             }else {
