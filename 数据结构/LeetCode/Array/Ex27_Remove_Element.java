2fd1c46e811c767b3bc3984811b5fa9c3092f072
Jayzyyzyy <xuweijay@163.com>
2017-08-28 16:45:58 +0800

    LeetCode

--- 数据结构/LeetCode/Array/Ex27_Remove_Element.java
+++ 数据结构/LeetCode/Array/Ex27_Remove_Element.java
@@ -16,8 +16,12 @@ public class Ex27_Remove_Element {
     public int removeElement(int[] nums, int val) {
         if(nums == null || nums.length == 0 ) return 0;
 
-
-
-
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j] != val){
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
     }
 }
