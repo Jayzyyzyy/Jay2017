0a0e7b3e08cf1236d5ee99dae3e444a08a9125c0
JayXu <xuweijay@163.com>
2018-03-21 10:57:45 +0800

    IDEA快捷键的练习文件

---
+++ IDEA快捷键/练习文件/A.java
@@ -0,0 +1,30 @@
+package 练习文件;
+
+/**
+ * Created by Jay on 2018/3/21
+ */
+public class A {
+
+    private int age = 10;
+
+    public void fa(){
+        System.out.println("abcdef");
+        System.out.println("faaaa");
+        System.out.println("faaaa");
+        System.out.println("faaaaccc");
+    }
+
+    public void f2(){
+        System.out.println("faaaa");
+        System.out.println("faaaa");
+        System.out.println("faaaa");
+        System.out.println("faaaa");
+    }
+
+    public void fsasas(){
+        System.out.println("faaaa");
+        System.out.println("faaaa");
+        System.out.println("faaaa");
+        System.out.println("faaaa");
+    }
+}
---
+++ IDEA快捷键/练习文件/B.java
@@ -0,0 +1,12 @@
+package 练习文件;
+
+/**
+ * Created by Jay on 2018/3/21
+ */
+public class B {
+
+    public void f1(){
+        System.out.println("aaaaafbbbbb");
+    }
+
+}
